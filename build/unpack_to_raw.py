610464bfd84fc65f1af0a7fa7d141043700bd61f
NoeBrt <noe.breton@efrei.net>
2024-12-04 13:49:57 +0100

    add unpack_to_raw

--- build/unpack_to_raw.py
+++ build/unpack_to_raw.py
@@ -2,9 +2,11 @@
 
 import os
 import pandas as pd
+import boto3
+import io
+from datetime import datetime
 
-
-def unpack_data(input_dir, output_file):
+def unpack_data(input_dir, output_file,client,bucket_name):
     """
     Unpacks and combines multiple CSV files from a directory into a single CSV file.
 
@@ -13,27 +15,57 @@ def unpack_data(input_dir, output_file):
     output_file (str): Path to the output combined CSV file.
     """
     data_frames = []
-    for file_name in os.listdir(input_dir):
-        if file_name.endswith('.csv') or 'data-' in file_name:
-            file_path = os.path.join(input_dir, file_name)
-            data = pd.read_csv(
-                file_path,
-                names=['sequence', 'family_accession', 'sequence_name', 'aligned_sequence', 'family_id']
-            )
-            data_frames.append(data)
+    for folder in os.listdir(input_dir):
+        if folder not in ["dev", "test", "train"]:
+            continue
+        for file_name in os.listdir(os.path.join(input_dir,folder)):
+            print(file_name)
+            if file_name.endswith('.csv') or 'data-' in file_name:
+                    file_path = os.path.join(input_dir,folder, file_name)
+                    data = pd.read_csv(
+                        file_path,
+                        names=['family_id','sequence_name','family_accession','aligned_sequence','sequence'],header=0
+                    )
+                    data_frames.append(data)
+    
     combined_data = pd.concat(data_frames, ignore_index=True)
+    os.makedirs(os.path.dirname(output_file), exist_ok=True)
     combined_data.to_csv(output_file, index=False)
+    try : 
+        send_file(output_file,client,bucket_name)
+        print(f"File {output_file} sent successfully to {bucket_name} at {datetime.now()}")
+    except Exception as e :
+        print(f"{output_file} Sending failed : {e} ")
+        
+        
 
+
+def init_client():
+    s3 = boto3.client(
+        "s3",
+        endpoint_url=os.getenv("AWS_ENDPOINT_URL", "http://localhost:4566"),
+        aws_access_key_id=os.getenv("AWS_API_KEY"),  # Default for LocalStack
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),  # Default for LocalStack
+    )
+    return s3
+
+
+def send_file(input_file,client,bucket_name):
+    client.upload_file(input_file, bucket_name, os.path.basename(input_file))
+
+     
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="Unpack and combine protein data")
     parser.add_argument("--input_dir", type=str, required=True, help="Path to input directory")
-    parser.add_argument("--output_file", type=str, required=True, help="Path to output combined CSV file")
-    args = parser.parse_args()
+    parser.add_argument("--output_file_name", type=str, required=True, help="Path to output combined CSV file")
+    parser.add_argument("--bucket_name", type=str, required=True, help="s3 bucket")
 
-    unpack_data(args.input_dir, args.output_file)
+    args = parser.parse_args()
+    s3=init_client()
+    unpack_data(args.input_dir, args.output_file_name,s3,args.bucket_name)
 
 #######################################################
 # Task: Update this code to handle the following:
